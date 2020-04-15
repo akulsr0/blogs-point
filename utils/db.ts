1b94848bb6c095a0f445214bfadc901ff57e7597
Akul Srivastava <akulsrivastava@Akuls-MacBook-Air.local>
2020-04-15 17:28:44 +0530

    depoy

--- utils/db.ts
+++ utils/db.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
 import config from 'config';
 
+const dburl: string = process.env.dburl || config.get('DB_URL');
+
 mongoose.connect(
-  config.get('DB_URL'),
+  dburl,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log(`Database Connected`);
